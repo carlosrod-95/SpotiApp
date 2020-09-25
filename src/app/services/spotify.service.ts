afeee98f7ab1d2daa594cdf27becf8583149811c
Carlos <crodriguezperez95@gmail.com>
2020-09-25 12:24:51 +0200

    SpotiApp

---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor(private http: HttpClient) { 
+
+    console.log('Spotify service listo');
+    
+  }
+
+  getQuery(query: string) {
+
+    const url = `https://api.spotify.com/v1/${query}`;
+
+    const headers = new HttpHeaders({
+      'Authorization':'Bearer BQAjf_4Kop1wMHxOZV98smgtsGeaS24xv2SawnZ-2fdlO9cB9ljEUtvdwX6KGoabZbOhokwQbAYnikw-8Hg'
+    });
+
+    return this.http.get(url, { headers});
+
+  }
+
+  getNewReleases(){
+
+    return this.getQuery('browse/new-releases?limit=20')
+          .pipe( map( data => data['albums'].items));
+  
+  }
+
+  getArtistas(termino:string) {
+
+    return this.getQuery(`search?q=${termino}&type=artist&limit=15`)
+          .pipe(map(data =>  data['artists'].items));
+    
+  }
+
+  getArtista(id:string) {
+
+    return this.getQuery(`artists/${id}`);
+         // .pipe(map(data =>  data['artists'].items));
+    
+  }
+
+  getTopTracks(id:string) {
+
+    return this.getQuery(`artists/${id}/top-tracks?country=us`)
+         .pipe(map(data =>  data['tracks']));
+    
+  }
+}
+
